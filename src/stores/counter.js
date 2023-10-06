be2135912330944e82e046bfb8c879a089c8785d
thephoo2546 <thephoo7@gmail.com>
2023-10-06 22:43:32 +0700

    edit file

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,16 +4,16 @@ import { defineStore } from 'pinia'
 export const useProductStore = defineStore('product', {
   state: () => ({
     productList: [
-      { name: ' Dark Spot Bright Serum ', price: 789, image:'/src/assets/clear-dark.jpg'},
-      { name: ' Vitamin C Concentrate Super Serum  ', price: 589, image:'/src/assets/clear-c.jpg' },
-      { name: ' UV Sun Serum SPF50+ PA++++ ', price: 500, image:'/src/assets/clear-uv.jpg' },
-      { name: ' Acne Care Solution Cleanser ', price: 110, image:'/src/assets/clear-clean.jpg' },
-      { name: ' Acne Care Solution Facial Serum', price: 690, image:'/src/assets/clear-facialse.jpg' },
-      { name: ' Intensive Facial Black Mask', price: 140, image:'/src/assets/CLEAR NOSE Intensive.jpg' },
-      { name: ' Aloe Vera Soothing Brightening Gel', price: 49, image:'/src/assets/clear-gel.jpg' },
-      { name: ' Acne Gel Concentrate Solution Care ', price: 49, image:'/src/assets/CLEAR NOSE Acne Gel.jpg' },
-      { name: ' Acne Care Solution BB', price: 400, image:'/src/assets/BB.jpg' },
-      { name: ' Acne Care Solution BB 4g', price: 49, image:'/src/assets/bbsong.jpg' },
+      { name: ' Dark Spot Bright Serum ', price: 789, image:'/imgs/clear-dark.jpg'},
+      { name: ' Vitamin C Concentrate Super Serum  ', price: 589, image:'/imgs/clear-c.jpg' },
+      { name: ' UV Sun Serum SPF50+ PA++++ ', price: 500, image:'/imgs/clear-uv.jpg' },
+      { name: ' Acne Care Solution Cleanser ', price: 110, image:'/imgs/clear-clean.jpg' },
+      { name: ' Acne Care Solution Facial Serum', price: 690, image:'/imgs/clear-facialse.jpg' },
+      { name: ' Intensive Facial Black Mask', price: 140, image:'/imgs/CLEAR NOSE Intensive.jpg' },
+      { name: ' Aloe Vera Soothing Brightening Gel', price: 49, image:'/imgs/clear-gel.jpg' },
+      { name: ' Acne Gel Concentrate Solution Care ', price: 49, image:'/imgs/CLEAR NOSE Acne Gel.jpg' },
+      { name: ' Acne Care Solution BB', price: 400, image:'/imgs/BB.jpg' },
+      { name: ' Acne Care Solution BB 4g', price: 49, image:'/imgs/bbsong.jpg' },
     ],
   }),
   getters: {
